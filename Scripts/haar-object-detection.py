86735e369f92699ec2aed1ee8d76cb742c1e2ebe
Saransh Karira <saransh661@gmail.com>
2018-08-13 20:11:51 +0530

    restructuring

--- Scripts/haar-object-detection.py
+++ Scripts/haar-object-detection.py
@@ -1,7 +1,7 @@
 # import numpy as np
 import cv2
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface.xml')
+import os
+face_cascade = cv2.CascadeClassifier(os.path.join('..', 'src', 'haarcascade_frontalface.xml'))
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
